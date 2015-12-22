bcd373ee61ddae025095746707925e892db1705e
Filipe Caldas <fcaldas@canal-plus.com>
2015-12-22 14:14:38 +0100

    Fix installer for Ubuntu 15.10

--- installDep.sh
+++ installDep.sh
@@ -1,7 +1,7 @@
 sudo apt-get install g++-4.8 g++ git make libboost1.54-all-dev libssl-dev cmake gcc
 sudo apt-get install libopencv-dev
 sudo apt-get install apache2 php5 php5-curl
-sudo apt-get install dkms
+sudo apt-get install dkms libssl-dev
 sudo apt-get install libtesseract-dev tesseract-ocr tesseract-ocr-fra
 sudo dpkg -i BlackmagicSoftware/deb/amd64/desktopvideo_10.5a17_amd64.deb
 sudo dpkg -i BlackmagicSoftware/deb/amd64/desktopvideo-gui_10.5a17_amd64.deb
@@ -12,6 +12,7 @@ git clone https://git.codeplex.com/casablanca
 cd casablanca/Release
 mkdir build.release
 cd build.release
+CMAKE_CXX_COMPILER=g++-4.8
 CXX=g++-4.8 cmake .. -DCMAKE_BUILD_TYPE=Release
 make
 sudo make install
